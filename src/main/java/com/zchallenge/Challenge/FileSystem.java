31af4abbdd2409c502b70e64af62c2ef01ced57d
LucianoNicolasArrieta <lucianoarrieta077@gmail.com>
2025-07-08 15:20:59 -0300

    Estructura basica fs

---
+++ src/main/java/com/zchallenge/Challenge/FileSystem.java
@@ -0,0 +1,31 @@
+package com.zchallenge.Challenge;
+
+public class FileSystem {
+
+    String root = "/user/aplicacion";
+
+    public void mkdir(String path) {
+
+        // Verifico si existe el directorio,
+        // Si no existe lo creo
+
+    }
+
+    public void cd(String dirName) {
+        // Si es valido (existe), me muevo a el
+        // Si recibo .., vuelvo atras
+
+    }
+
+    public void touch(String fileName) {
+        // Si es valido, lo crea en el directorio donde se encuentra
+    }
+
+    public void ls() {
+        // Lista archivos y carpetas en el directorio actual
+    }
+
+    public void pwd() {
+        // Muestra el directorio/path actual.
+    }
+}
